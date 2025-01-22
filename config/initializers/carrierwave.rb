35d47fab472c52dd990a49c5d092c51afaea55e9
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-01-23 00:32:15 +0900

    fix:configを公式の書き方に変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,22 +1,23 @@
-require "carrierwave/storage/abstract"
-require "carrierwave/storage/file"
-require "carrierwave/storage/fog"
+# require "carrierwave/storage/abstract"
+# require "carrierwave/storage/file"
+# require "carrierwave/storage/fog"
 
 CarrierWave.configure do |config|
-  if Rails.env.production?
-    config.storage :fog
+  # if Rails.env.production?
+    # config.storage :fog
     # config.fog_provider = "fog/aws"
-    config.fog_directory  = ENV["S3_BUCKET_NAME"]
-    config.fog_public = false
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-      region: ENV["S3_REGION"]
+      region: 'ap-northeast-1'
       #   path_style: true
     }
-  else
-    config.storage :file # 分岐の処理はアップローダークラスに記載
-    config.enable_processing = false if Rails.env.test?
-  end
+    config.fog_directory  = "mini4wd"
+    config.fog_public = false
+    
+  # else
+    # config.storage :file # 分岐の処理はアップローダークラスに記載
+    # config.enable_processing = false if Rails.env.test?
+  # end
 end
