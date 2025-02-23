cf38e0fb11b5c3945551749bff0d623646f42071
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-02-23 22:03:12 +0900

    update:popup

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,13 @@
+//= require jquery
+//= require lightbox2
+
+lightbox.option({
+    'alwaysShowNavOnTouchDevices': true,
+    'wrapAround': true,
+    'albumLabel': "画像 %1 / %2",
+    'fadeDuration': 300,
+    'imageFadeDuration': 300,
+    'resizeDuration': 200,
+    'disableScrolling': false
+});
+  
