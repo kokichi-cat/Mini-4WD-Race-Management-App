b6ae1c30a44a591ec531ae905f1772b52293be68
nakamuranaoko <wara1203wara_pet@yahoo.co.jp>
2025-02-24 01:35:26 +0900

    fix:js修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,13 +1,6 @@
-//= require jquery
-//= require lightbox2
-
-lightbox.option({
-    'alwaysShowNavOnTouchDevices': true,
-    'wrapAround': true,
-    'albumLabel': "画像 %1 / %2",
-    'fadeDuration': 300,
-    'imageFadeDuration': 300,
-    'resizeDuration': 200,
-    'disableScrolling': false
+document.addEventListener("turbo:load", function() {
+    console.log("Turbo loaded - reinitializing Lightbox"); // デバッグ用
+    if (window.lightbox) {
+        lightbox.init(); // 再初期化
+    }
 });
-  
